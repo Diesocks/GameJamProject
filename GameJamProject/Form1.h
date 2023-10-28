ee397798c892421a260c8a92b80d6b12cfb69fee
unknown <diegovilcatut@gmail.com>
2023-10-27 22:45:56 -0500

    prueba en formulario

--- GameJamProject/Form1.h
+++ GameJamProject/Form1.h
@@ -34,6 +34,9 @@ namespace GameJamProject {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+	private: System::Windows::Forms::Label^ label1;
 
 	private:
 		/// <summary>
@@ -48,11 +51,40 @@ namespace GameJamProject {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Form1";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(214, 148);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(147, 58);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"aceptar";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(272, 117);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(29, 13);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"Hola";
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(613, 430);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button1);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
 	};
